15f5b04a673728e47de7c0f1821e7887207161c2
S ABHIRAM <sabhiram@Ss-MacBook-Air.local>
2026-06-22 11:45:57 +0530

    fix streamlit python path

--- auriseg-intern/src/ui/app.py
+++ auriseg-intern/src/ui/app.py
@@ -2,6 +2,8 @@ import inspect
 import json
 import sys
 from pathlib import Path
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+sys.path.append(str(PROJECT_ROOT))
 import pandas as pd
 import plotly.express as px
 
